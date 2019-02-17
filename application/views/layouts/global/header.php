c20cbc384d6302118f71f166c7fd5804961e121f
Rodrigo Arenas <arenasgonzalezrodrigo@gmail.com>
2019-02-17 17:02:10 -0600

    feat: Agregand archivos layouts globales

--- application/views/layouts/global/header.php
+++ application/views/layouts/global/header.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio de sesión</title>
+    <title>Panel de [*   *]</title>
 </head>
 <body>
-    <section class="c-main-login">
-            
-    </section>
-</body>
-</html>
+    
---
+++ application/views/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio de sesión</title>
+</head>
+<body>
+    <section class="c-main-login">
+        <div class="c-login-form">
+            <form action="<?php echo base_url();?> auth/login" method="POST">
+            <?php echo base_url(); ?>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
